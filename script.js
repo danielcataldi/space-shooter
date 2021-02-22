fc234c8502b717faf723ca41826c9d2121ffcbd6
Daniel <danielcataldi95@gmail.com>
2021-02-22 10:46:49 -0300

    html,css e script de movimento

---
+++ script.js
@@ -0,0 +1,73 @@
+const yourShip = document.querySelector('.player-shooter');
+const playArea = document.querySelector('#main-play-area');
+
+//captar movimento e tiro da nave
+function flyShip(event) {
+    if(event.key === 'ArrowUp') {
+        event.preventDefault()
+        moveUp()
+    } else if(event.key === 'ArrowDown') {
+        event.preventDefault()
+        moveDown();
+    } else if(event.key === " ") {
+        event.preventDefault()
+        fireLaser()
+    }
+}
+
+//função para subir
+function moveUp() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top')
+    if(topPosition === "0px") {
+        return
+    } else {
+        let position = parseInt(topPosition)
+        position -= 50
+        yourShip.style.top = `${position}px`
+    }
+}
+
+//função para descer
+function moveDown() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top')
+    if(topPosition === "500px"){
+        return
+    } else {
+        let position = parseInt(topPosition)
+        position += 50
+        yourShip.style.top = `${position}px`
+    }
+}
+
+//funcao atirar
+function fireLaser(){
+    let laser = createLaserElement()
+    playArea.appendChild(laser)
+    moveLaser()
+}
+
+function createLaserElement(){
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'))
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'))
+    let newLaser = document.createElement('img')
+    newLaser.src = './assets/shoot.png'
+    newLaser.classList.add('laser')
+    newLaser.style.left = `${xPosition}px`
+    newLaser.style.top = `${yPosition - 10}px`
+    return newLaser
+}
+
+function moveLaser(){
+    
+}
+
+window.addEventListener('keydown',flyShip)
+
+
+
+
+
+
+
+
+
